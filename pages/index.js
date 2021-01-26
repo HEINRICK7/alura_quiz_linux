2c583f41e7d073f95387d3d6c99739c31d36c9eb
Henrique <Carlos Henrique>
2021-01-26 17:25:00 -0300

    projeto criado imersão alura

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,59 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
+
+/*const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1; 
+
+  h1 {
+    font-family: 'MrRobot';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 50px;
+    text-align: center;
+    color: #e5627c;
+  }
+`;*/
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  margin-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding:15px;
+  }
+`;
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    < QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+             <h1>mr.Robot</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+         <Widget.Content>
+            <h1>Quiz mr.Robot</h1>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/HEINRICK7/alura_quiz_mr.robot" />
+    </QuizBackground>
+  )
+
 }
---
+++ src/components/Widget/index.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+const Widget = styled.div`
+  margin-top: 90px;
+  margin-bottom: 0;
+  border: 1px solid #e5627c;
+  background-color: #1C1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+    color: #ffff;
+    
+  }
+
+  p {
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 1;
+    color: snow;
+
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({theme}) => theme.colors.primary};
+  * {
+    margin: 0;
+  }
+`;
+
+export default Widget;
